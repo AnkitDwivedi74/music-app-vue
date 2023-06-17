d5e93f596f4ceb289ee3e6766accdb91545707ee
AnkitDwivedi74 <andwivedi74@gmail.com>
2023-06-17 11:24:10 +0530

    application

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -4,11 +4,18 @@ import "firebase/firestore";
 import "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDvF17Y5ApfuVBSm_bemsZYoPrNVUT1r2s",
-  authDomain: "music-temp-2a2ab.firebaseapp.com",
-  projectId: "music-temp-2a2ab",
-  storageBucket: "music-temp-2a2ab.appspot.com",
-  appId: "1:513389509498:web:f976453109bb87de1034c3",
+  apiKey: "AIzaSyBQOR_9LBX_FHl3rUboQSybM1YZcyA_Z-U",
+  authDomain: "music-app-d64a8.firebaseapp.com",
+  projectId: "music-app-d64a8",
+  storageBucket: "music-app-d64a8.appspot.com",
+  messagingSenderId: "929684874091",
+  appId: "1:929684874091:web:fe678a5faf9fc7a4efa22e",
+  measurementId: "G-LWV6S8QR1L"
+  // apiKey: "AIzaSyDvF17Y5ApfuVBSm_bemsZYoPrNVUT1r2s",
+  // authDomain: "music-temp-2a2ab.firebaseapp.com",
+  // projectId: "music-temp-2a2ab",
+  // storageBucket: "music-temp-2a2ab.appspot.com",
+  // appId: "1:513389509498:web:f976453109bb87de1034c3",
 };
 
 // Initialize Firebase
